646c17618e089f891dbe9085192970a03cdce62f
victor.santos <victor.santos@pdcase.com>
2022-12-13 19:35:53 -0300

    Aula 23 next Propriedade em componente

--- components/Titulo.jsx
+++ components/Titulo.jsx
@@ -1,8 +1,13 @@
-export default function Titulo() {
-    return (
-        <>
-            <h1>Título</h1>
-            <h2>Subtítulo</h2>
-        </>
-    )
-}
+export default function Titulo(props) {
+        return props.pequeno ? (
+            <>
+                <p>{props.principal}</p>
+                <p>{props.secundario}</p>
+            </>
+        ) : (
+            <>
+                <h1>{props.principal}</h1>
+                <h2>{props.secundario}</h2>
+            </>
+        )
+    }
