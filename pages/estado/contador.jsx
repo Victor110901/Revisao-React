a3e6cd9084d8d8eb3a0dc4d1665aa545d8c9d5a7
victor.santos <victor.santos@pdcase.com>
2022-12-15 16:42:14 -0300

    Aula 42 next Desafio Contado

---
+++ pages/estado/contador.jsx
@@ -0,0 +1,9 @@
+export default function contador() {
+    return (
+        <div>
+            <div>Valor: ???</div>
+            <button>-</button>
+            <button>+</button>
+        </div>
+    )
+}
