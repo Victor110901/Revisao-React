006cc988877c9c72c54c5958407f504de099bc52
victor.santos <victor.santos@pdcase.com>
2022-12-16 11:14:25 -0300

    Aula 48 next Correção de warm com useEffect

--- pages/estado/megasena.jsx
+++ pages/estado/megasena.jsx
@@ -1,11 +1,15 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import NumeroDisplay from "../../components/NumeroDisplay"
 import { mega } from '../../functions/mega'
 
 export default function megasena() {
 
     const [qtde, setQtde] = useState(6)
-    const [numeros, setNumeros] = useState(mega())
+    const [numeros, setNumeros] = useState([])
+
+    useEffect(() => {
+        setNumeros(mega())
+    }, [])
 
     function renderizarNumeros() {
         return numeros.map(
